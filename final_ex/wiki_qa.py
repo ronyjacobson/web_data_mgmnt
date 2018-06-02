7c010fa8fd6c42db24c4cf1d217e7ad9fad1dac5
Rony Jacobson <ronyjacobson@gmail.com>
2018-06-02 17:48:51 +0300

    list items working

--- final_ex/wiki_qa.py
+++ final_ex/wiki_qa.py
@@ -6,6 +6,7 @@ from lxml import etree
 import sys
 import rdflib
 import distance
+from collections import defaultdict
 
 DEBUG = True
 
@@ -78,23 +79,33 @@ def get_wikilink(entity):
 
 def get_infobox_relations(wikilink):
     ''' The function receives a wikilink, fetches it's contents and extracts all relations in the infobox'''
-    relations = []
+    relations = defaultdict(list)
 
-    TH_XPATH = "//table[contains(@class,'infobox')]/tr[{}]/th//text()"
-    TD_XPATH = "//table[contains(@class,'infobox')]/tr[{}]/td//text()"
+    TH_XPATH = "//table[position()=1 and contains(@class,'infobox')]/tr[{}]/th//text()"
+    TD_LI_XPATH = "//table[position()=1 and contains(@class,'infobox')]/tr[{}]/td//li[{}]//text()"
+    TD_XPATH = "//table[position()=1 and contains(@class,'infobox')]/tr[{}]/td//text()"
 
     req = requests.get(wikilink)
     doc = lxml.html.fromstring(req.content)
     infobox_rows = doc.xpath("//table[contains(@class,'infobox')]/tr")
-    for i in range(0, len(infobox_rows)):
+    for i in range(1, len(infobox_rows)+1):
         relation = ''.join(doc.xpath(TH_XPATH.format(i))).replace('\n', ' ').strip().encode('utf-8')
-        value = ''.join(doc.xpath(TD_XPATH.format(i))).replace('\n', ' ').strip().encode('utf-8')
-        if relation != '' and value != '':
-            relations.append([relation, value])
-    if DEBUG:
-        print("relations:")
-        for relation, value in relations:
-            print "{} : {}".format(relation, value)
+
+        values = doc.xpath("//table[position()=1 and contains(@class,'infobox')]/tr[{}]/td//li".format(i))
+        if len(values) == 0:
+            value = ''.join(doc.xpath(TD_XPATH.format(i))).replace('\n', ' ').strip().encode('utf-8')
+            if relation != '' and value != '':
+                relations[relation].append(value)
+        else:
+            for j in range(1, len(values)+1):
+                value = ''.join(doc.xpath(TD_LI_XPATH.format(i,j))).replace('\n', ' ').strip().encode('utf-8')
+                if relation != '' and value != '':
+                    relations[relation].append(value)
+
+    # if DEBUG:
+    #     print("relations:")
+    #     for relation in relations:
+    #         print "{} : {}".format(relation, relations[relation])
 
     return relations
 
@@ -103,16 +114,16 @@ def extract_query_answer(relations, query_relation):
     ''' Take the closest relation to the one asked in the query using Jaccard word distance and return it's value and
     it's name as it will be in in the ontology. '''
     min_word_distance = float('inf')
-    requested_relation_answer = ""
+    requested_relation_answers = []
     relation_name = ""
 
-    for relation, value in relations:
+    for relation in relations.keys():
         relation_word_distance = distance.jaccard(relation.lower(), query_relation.lower())
         if relation_word_distance < min_word_distance:
-            requested_relation_answer = value
+            requested_relation_answers = relations[relation]
             relation_name = relation
             min_word_distance = relation_word_distance
-    return requested_relation_answer, relation_name
+    return requested_relation_answers, relation_name
 
 
 def build_ontology(entity, relations):
@@ -122,10 +133,11 @@ def build_ontology(entity, relations):
     g = rdflib.Graph()
     entity_ref = rdflib.URIRef("http://example.org/" + entity.replace(" ", "_"))
 
-    for relation, value in relations:
+    for relation in relations:
         relation_ref = rdflib.URIRef("http://example.org/" + relation.replace(" ", "_"))
-        value_ref = rdflib.URIRef("http://example.org/" + value.replace(" ", "_"))
-        g.add((entity_ref, relation_ref, value_ref))
+        for value in relations[relation]:
+            value_ref = rdflib.URIRef("http://example.org/" + value.replace(" ", "_"))
+            g.add((entity_ref, relation_ref, value_ref))
 
     g.serialize(ONTOLOGY_FILE, format="nt")
     sorted(g)
@@ -154,20 +166,17 @@ def run_query_on_ontology():
 
 
 def wiki_qa_main(query):
-    try:
-        input = process_input(query)
-        print_debug("Parsed input is: " + input.__str__())
-        wikilink = get_wikilink(input[ENTITY].replace(" ", "_"))
-        relations = get_infobox_relations(wikilink)
-        answer, relation_name = extract_query_answer(relations, input[RELATION])
-        print ("Answer is: " + answer)
-        build_ontology(input[ENTITY], relations)
-        translate_query_to_sparql(relation_name, input[ENTITY])
-        if DEBUG:
-            run_query_on_ontology()
-        return 0
-    except:
-        return 1
+    input = process_input(query)
+    print_debug("Parsed input is: " + input.__str__())
+    wikilink = get_wikilink(input[ENTITY].replace(" ", "_"))
+    relations = get_infobox_relations(wikilink)
+    answers, relation_name = extract_query_answer(relations, input[RELATION])
+    print ("Answer is: {}".format(answers))
+    build_ontology(input[ENTITY], relations)
+    translate_query_to_sparql(relation_name, input[ENTITY])
+    if DEBUG:
+        run_query_on_ontology()
+    return 0
 
 
 def main(argv):
