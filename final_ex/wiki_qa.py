c4646011e7ea8d37c62c7997d80e5d659287bd7a
Rony Jacobson <ronyjacobson@gmail.com>
2018-06-05 14:09:44 +0300

    Compare only alphanumeric characters for detecting relations

--- final_ex/wiki_qa.py
+++ final_ex/wiki_qa.py
@@ -9,6 +9,7 @@ import distance
 from collections import defaultdict
 
 DEBUG = False
+DEBUG_QUERIES = True
 
 # Consts
 RELATION = 'relation'
@@ -17,7 +18,7 @@ ONTOLOGY_FILE = 'ontology.nt'
 QUERY_FILE = 'query.sparql'
 
 
-queries = [
+debug_queries = [
     "Who is the president of Italy?",
     "Who is the spouse of Gal Gadot?",
     "What is the alma mater of Gal Gadot?",
@@ -79,7 +80,6 @@ def get_infobox_relations(wikilink):
 
     for i in range(1, len(infobox_rows)+1):
         relation = ''.join(doc.xpath(TH_XPATH.format(i))).replace('\n', ' ').strip().encode('utf-8')
-
         # Check if there are multiple values (a list)
         values = doc.xpath("//table[position()=1 and contains(@class,'infobox')]/tr[{}]/td//li".format(i))
         if len(values) == 0:
@@ -95,7 +95,8 @@ def get_infobox_relations(wikilink):
     if DEBUG:
         print("relations:")
         for relation in relations:
-            print "{} : {}".format(relation, relations[relation])
+            for value in relations[relation]:
+                print("{} : {}".format(relation, value))
 
     if len(relations) == 0:
         raise Exception("Error: Could not find relations for wikilink: [{}]".format(wikilink))
@@ -111,7 +112,12 @@ def extract_query_answer(relations, query_relation):
     relation_name = ""
 
     for relation in relations.keys():
-        relation_word_distance = distance.levenshtein(relation.lower(), query_relation.lower())
+        # Compare only alpha numeric characters
+        clean_relation = re.sub(r'[^a-zA-Z123456789\s]', "", relation.lower())
+        clean_query_relation = re.sub(r'[^a-zA-Z123456789\s]', "", query_relation.lower())
+
+        relation_word_distance = distance.levenshtein(clean_relation, clean_query_relation)
+        # print "distance {}<-->{} is {}".format(clean_query_relation, clean_relation, relation_word_distance)
         if relation_word_distance < min_word_distance:
             requested_relation_answers = relations[relation]
             relation_name = relation
@@ -121,12 +127,12 @@ def extract_query_answer(relations, query_relation):
 
 def build_ontology(entity, relations):
     g = rdflib.Graph()
-    entity_ref = rdflib.URIRef("http://example.org/" + entity.replace(" ", "_"))
+    entity_ref = rdflib.URIRef("http://example.org/{}".format(entity.replace(" ", "_")))
 
     for relation in relations:
-        relation_ref = rdflib.URIRef("http://example.org/" + relation.replace(" ", "_"))
+        relation_ref = rdflib.URIRef("http://example.org/{}".format(relation.replace(" ", "_")))
         for value in relations[relation]:
-            value_ref = rdflib.URIRef("http://example.org/" + value.replace(" ", "_"))
+            value_ref = rdflib.URIRef("http://example.org/{}".format(value.replace(" ", "_")))
             g.add((entity_ref, relation_ref, value_ref))
 
     g.serialize(ONTOLOGY_FILE, format="nt")
@@ -161,7 +167,7 @@ def wiki_qa_main(query):
     wikilink = get_wikilink(input[ENTITY].replace(" ", "_"))
     relations = get_infobox_relations(wikilink)
     answers, relation_name = extract_query_answer(relations, input[RELATION])
-    print ("Answer is: {}".format(answers))
+    print ("Query: {}\nWikipedia's value/s for {} relation is: {}".format(query, relation_name, answers))
     build_ontology(input[ENTITY], relations)
     translate_query_to_sparql(relation_name, input[ENTITY])
     if DEBUG:
@@ -170,8 +176,8 @@ def wiki_qa_main(query):
 
 
 def main(argv):
-    if DEBUG:
-        for query in queries:
+    if DEBUG_QUERIES:
+        for query in debug_queries:
             wiki_qa_main(query)
             print ("*************************************")
 
